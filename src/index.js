8ecd4942cf946f4adb7485c5a39421b0b7100eca
abuzarhamza <abuzarhamza@gmail.com>
2019-11-02 23:31:46 -0500

    solution

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//import s
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props); 
+        this.state = {
+            lat: null,
+            lon: null,
+            errorMessage: null,
+        };
+        window.navigator.geolocation.getCurrentPosition((pos)=> {
+            this.setState({lat: pos.coords.latitude, lon: pos.coords.longitude});
+        }, (err)=> {
+            this.setState({errorMessage: err.message});
+        })
+    }
+
+    render() {
+        if (this.state.lat) {
+            return(
+                <div>
+                    lat:  {this.state.lat} , lon: {this.state.lon}
+                </div>
+            );
+        } else if (this.state.errorMessage)  {
+            return <div> error : {this.state.errorMessage}</div>;
+        } else {
+            return <div>Loading....</div>;
+        }
+    }
+}
+ReactDom.render(<App />, document.querySelector('#root'));
